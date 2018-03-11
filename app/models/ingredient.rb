49750a695f6188fcecec3f1d11fd4a030bc71db6
Raghu Betina <raghu@firstdraft.com>
2018-03-11 23:03:37 +0000

    Direct association between ingredients and ingredient_ascs

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,9 @@
 class Ingredient < ApplicationRecord
   # Direct associations
 
+  has_many   :ingredient_ascs,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/ingredient_asc.rb
+++ app/models/ingredient_asc.rb
@@ -1,6 +1,8 @@
 class IngredientAsc < ApplicationRecord
   # Direct associations
 
+  belongs_to :ingredient
+
   belongs_to :recipe,
              :foreign_key => "r_id"
 
