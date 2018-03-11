cd689ae264c8736bed98c83f0b24a24a5a3a58c5
Raghu Betina <raghu@firstdraft.com>
2018-03-11 23:03:29 +0000

    Validate uniqueness of likes.recipe_id

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,6 +5,8 @@ class Like < ApplicationRecord
 
   # Validations
 
+  validates :recipe_id, :uniqueness => { :scope => [:user_id], :message => "already liked" }
+
   validates :recipe_id, :presence => true
 
   validates :user_id, :presence => true
